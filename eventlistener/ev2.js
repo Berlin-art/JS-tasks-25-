a96fe4899353708521cdc274425e5a61312e5c84
Berlin-art <michealberlina@gmail.com>
2024-08-05 20:34:13 +0530

    Add files via upload

---
+++ eventlistener/ev2.js
@@ -0,0 +1,6 @@
+const keyDisplay = document.getElementById('key-display');
+
+// Add event listener to capture key presses
+document.addEventListener('keydown', event => {
+    keyDisplay.textContent = `Key Code: ${event.keyCode}`;
+});
